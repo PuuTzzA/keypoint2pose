56928130ab39c71703e6284501cb08a97ed84929
PuuTzzA <putzerthomas03@gmail.com>
2024-10-29 13:46:21 +0900

    create view frustum visualizer and transform to NDC

--- obj2dots.py
+++ obj2dots.py
@@ -19,10 +19,23 @@ bpy.ops.object.delete()
 
 model_matrix = np.array([
     [1, 0, 0, 0],
-    [0, 1, 0, 9],
-    [0, 0, 1, -1],
+    [0, 1, 0, -1],
+    [0, 0, 1, 9],
     [0, 0, 0, 1]
 ])
+
+
+def set_camera_params():
+    camera = bpy.context.scene.camera
+    focal_length = camera.data.lens
+    sensor_width = camera.data.sensor_width
+    n = camera.data.clip_start
+    f = camera.data.clip_end
+    
+    render = bpy.context.scene.render
+    resolution = (render.resolution_x, render.resolution_y)
+
+    return focal_length, sensor_width, n, f, resolution
 
 
 # https://www.scratchapixel.com/lessons/3d-basic-rendering/3d-viewing-pinhole-camera/implementing-virtual-pinhole-camera.html
@@ -38,26 +51,81 @@ def compute_frustum(focal_length, sensor_width, near, resolution):
     t = near * math.tan(fov_vertical / 2)
     b = -t
 
-    return l, r, t, b
+    return l, r, t, b, fov, fov_vertical
+
+
+def create_view_frustum_visualizer(l, r, t, b, n, f, fov, fov_vertical):
+    # Create a new cube
+    bpy.ops.mesh.primitive_cube_add()
+    cube = bpy.context.object
+    cube.name = "ViewFrustum"
+
+    # Enter edit mode to modify the vertices
+    #bpy.ops.object.mode_set(mode='EDIT')
+    #bpy.ops.mesh.select_all(action='SELECT')
+
+    # Access the mesh data
+    mesh = cube.data
+
+    # Set the vertex coordinates
+    r_far = f * math.tan(fov / 2)
+    l_far = -r_far
+    t_far = f * math.tan(fov_vertical / 2)
+    b_far = -t_far
+    vertex_coords = [
+        (r, t, n),
+        (r, b, n),
+        (l, t, n),
+        (l, b, n),
+        (r_far, t_far, f),
+        (r_far, b_far, f),
+        (l_far, t_far, f),
+        (l_far, b_far, f),
+    ]
+
+    for i, coord in enumerate(vertex_coords):
+       mesh.vertices[i].co = coord
+
+    # Return to object mode
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    # Add and apply the Wireframe modifier
+    cube.modifiers.new(name="Wireframe", type='WIREFRAME')
+    #bpy.ops.object.modifier_apply(modifier=wireframe_modifier.name)
+
+    bpy.ops.mesh.primitive_cube_add(size=2)
+    cube = bpy.context.object
+    cube.name = "NDC"
+    cube.modifiers.new(name="Wireframe", type='WIREFRAME')
+
 
 n = 1
-f = 10
+f = 15
 
 focal_length = 30
 sensor_width = 36
 resolution = (1920, 1080)
 
-l, r, t, b = compute_frustum(focal_length, sensor_width, n, resolution)
 
-bpy.ops.mesh.primitive_cube_add(size=0.2, location=(r, n, t))
-bpy.ops.mesh.primitive_cube_add(size=0.2, location=(l, n, b))
+focal_length, sensor_width, n, f, resolution = set_camera_params()
+l, r, t, b, fov, fov_vertical = compute_frustum(focal_length, sensor_width, n, resolution)
 
 
-m_p = np.array([
+create_view_frustum_visualizer(l, r, t, b, n, f, fov, fov_vertical)
+
+
+m_p_naive = np.array([
     [1, 0, 0, 0],
     [0, 1, 0, 0],
     [0, 0, 1, 0],
-    [0, 1, 0, 0]
+    [0, 0, 1, 0]
+])
+
+m_p = np.array([
+    [n, 0, 0, 0],
+    [0, n, 0, 0],
+    [0, 0, n+f, -n*f],
+    [0, 0, 1, 0]
 ])
 
 m_o = np.array([
@@ -84,9 +152,16 @@ with open(obj_file_path, 'r') as file:
             # Place a cube at each vertex position
             bpy.ops.mesh.primitive_cube_add(size=0.5, location=(location[0], location[1], location[2]))
             
-            location = m_p @ location
+            location2 = location.copy()
+
+            location = m_p_naive @ location
             location /= location[3]
 
-            bpy.ops.mesh.primitive_cube_add(size=0.01, location=(location[0], location[1], location[2]))
+            #bpy.ops.mesh.primitive_cube_add(size=0.01, location=(location[0], location[1], location[2]))
+
+            location2 = m_o @ m_p @ location2
+            location2 /= location2[3]
+
+            bpy.ops.mesh.primitive_cube_add(size=0.1, location=(location2[0], location2[1], location2[2]))
 
 
